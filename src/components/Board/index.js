5ae4aa37a4848d9711bbc0d0fd63a1ee79eba310
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-12-21 12:38:57 -0700

    imported MUI

---
+++ src/components/Board/index.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+export default function Board () {
+    return (
+        <h1>Test Board</h1>
+    )
+}
